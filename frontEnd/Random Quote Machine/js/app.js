0e6b69d0aa8d743f8a2b15671d94456938984bae
Nicholas <niklasoti@gmail.com>
2016-08-27 04:49:43 +0200

    modifications

--- frontEnd/Random Quote Machine/js/app.js
+++ frontEnd/Random Quote Machine/js/app.js
@@ -15,21 +15,24 @@ var main = function(){
     });
   }
 
-  function addQuote(res) {           
-    $quote.html('');
-    $author.html('');       
-    $quote.append(res.quoteText? res.quoteText : 'Error, please try again');
-    $author.append(res.quoteAuthor? res.quoteAuthor : 'Anonymous');   
+  function addQuote(res) {  
+    
+    // Clear and append new quote and author    
+    $quote.html('').append(res.quoteText? res.quoteText : 'Error, please try again');
+    $author.html('').append(res.quoteAuthor? res.quoteAuthor : 'Anonymous'); 
+    
+    // Set links  
     $quote.attr('cite', res.quoteLink? res.quoteLink : '#');
-    $('a').attr('href', res.quoteAuthor? "https://en.wikipedia.org/wiki/"+res.quoteAuthor : '#');
+    $('#authorLink').attr('href', res.quoteAuthor? "https://en.wikipedia.org/wiki/"+res.quoteAuthor : '#');
+    $('#tweet').attr('href','https://twitter.com/intent/tweet?text='+res.quoteText+' -'+res.quoteAuthor+' #Quotes'); 
   }
 
-  getQuote();
-
-  $('#getQuoteBtn').on('click',function(){    
+  $('#newQuotebtn').on('click',function(){    
     $blockquote.fadeOut(1200,getQuote); 
     $blockquote.fadeIn(1200);
   });
+
+  getQuote();
 };
 
 $(document).ready(main);
