54c67b5eaac7923052d37ae2d6fcaf11e81000cd
Nicholas <niklasoti@gmail.com>
2016-09-01 23:22:16 +0200

    MVC style -Separation of concerns

--- frontEnd/Local Weather/js/app.js
+++ frontEnd/Local Weather/js/app.js
@@ -1,100 +1,88 @@
-var main = function(){
+'use strict';
 
-	'use strict';
+/*Model*/
+var data = {
 
-
-  function getData(current_Url,forecast_Url){
-    
-    //loading two Ajax requests concurrently with one callback
+  getWeatherData: function(current_Url,forecast_Url) {
+  
     $.when(
-        $.getJSON(current_Url), 
-        $.getJSON(forecast_Url)
+      $.getJSON(current_Url), 
+      $.getJSON(forecast_Url)
     ).then(
-    function(currentData,forecastData) { 
-      console.log(currentData[0]);
-      console.log(forecastData[0]);
-      logCurrent(currentData[0]);
-      view(currentData[0],forecastData[0]);
-    });
-  }
+      function(current,forecast){
+        //console.log(current);
+        //console.log(forecast);
+        handler.handleCurrentData(current);
+        handler.handleForecastData(forecast);
+      });
+  },
+};
 
-  function getGeoBasedWeather(lat,long) {	  		  		        
-    
+
+/*Controller*/
+var handler = {
+  
+  getGeoWeather: function(lat,long) {                       
+   
     let current_Url='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+
-    '&appid=8ebe5fef05ebd9a45403357e1ef5a16c';	
-    
+    '&appid=8ebe5fef05ebd9a45403357e1ef5a16c';  
+   
     let forecast_Url='http://api.openweathermap.org/data/2.5//forecast/daily?lat='+lat+'&lon='+long+
-    '&cnt=7&appid=8ebe5fef05ebd9a45403357e1ef5a16c';			  
+    '&cnt=7&appid=8ebe5fef05ebd9a45403357e1ef5a16c';           
     
-    getData(current_Url,forecast_Url);
-  }
+    data.getWeatherData(current_Url,forecast_Url);
+  },
 
-  function getCityBasedWeather(cityName){ 
-  	
+  getCityWeather: function(cityName){  
+    
     let current_Url='http://api.openweathermap.org/data/2.5/weather?q='+cityName+
-  	'&appid=8ebe5fef05ebd9a45403357e1ef5a16c';  
+    '&appid=8ebe5fef05ebd9a45403357e1ef5a16c';    
     
     let forecast_Url='http://api.openweathermap.org/data/2.5/forecast/daily?q='+cityName+
     '&cnt=7&appid=8ebe5fef05ebd9a45403357e1ef5a16c'; 
-
-    getData(current_Url,forecast_Url);
-  }
-  
-  function ipGeoLocator(){
-    $.get("http://ipinfo.io", function(response) {    
-      var location = response.loc.split(/,/);
-      let lat = location[0];
-      let long = location[1];
-      getGeoBasedWeather(lat,long);
-    },"jsonp");
-  }
-  
-  function clientGeoLocator(position){      
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    getGeoBasedWeather(lat,long);  
-  }
-
-  function logCurrent(res) {
-    console.log('LOCATION:')
-    console.log('City: '+res.name);
-    console.log('Country: '+res.sys.country);
-    console.log('Coordinates: '+ res.coord.lat +' , '+ res.coord.lon);
-    console.log('WEATHER:')
-    console.log('Description: '+res.weather[0].description);
-    console.log('Weather icon: '+ res.weather[0].icon);
-    console.log('Weather id: '+ res.weather[0].id);
-    console.log('Main: '+ res.weather[0].main);
-    console.log('DETAILS:')
-    console.log('Humidity: '+res.main.humidity);  
-    console.log('Pressure: '+res.main.pressure); 
-    console.log('Temp: '+res.main.temp); 
-    console.log('Max temp: '+res.main.temp_max);
-    console.log('Min temp: '+res.main.temp_min); 
-  }
-
-  /*function logForecast(res) {
-    console.log(res); 
-  }*/
-
-  function view(current,forecast){
     
-    var temperature=current.main.temp-273.15;
-    var icon = current.weather[0].icon;
-    var description = current.weather[0].description;
-    var location = current.name;
-    var mainWeather = current.weather[0].main;
-    
+    data.getWeatherData(current_Url,forecast_Url);
+  },
 
-    //Clear divs
+  handleCurrentData: function(current){
+    
+    //Handle current data and update view
+    current = current[0];
+
+    let tmp=current.main.temp-273.15;
+    let icon = current.weather[0].icon;
+    let description = current.weather[0].description;
+    let location = current.name;
+    let main = current.weather[0].main;
+    view.displayCurrentWeather(tmp,icon,description,location,main);
+
+    let temp_max = (current.main.temp_max-273.15).toFixed()
+    let temp_min = (current.main.temp_min-273.15).toFixed()
+    let humidity = current.main.humidity;
+    let pressure = current.main.pressure;
+    view.displayWeatherDetails(temp_max,temp_min,humidity,pressure);  
+  },
+
+  handleForecastData: function(forecast){
+    forecast = forecast[0];
+    view.displayForecastData(forecast);
+  }
+};
+
+
+/*View*/
+var view = {
+
+  displayCurrentWeather: function(tmp,icon,description,location,main){
+
+    //Clear div
     var $weather = $('#weather').html(''); 
-    var $details = $('#details').html('');
 
     //Get temp color
     var tempColor;
-    if(temperature<15){
+    if(tmp<15){
       tempColor = 'cold';
-    } else if(temperature < 25){
+    } else if(tmp < 25){
       tempColor = 'warm';
     }else{
       tempColor ='hot';
@@ -103,94 +91,223 @@ var main = function(){
     //Weather info
     var $weatherInfo = $('<div>');
     $weatherInfo.addClass(tempColor);
-    //Location
+    
     var $location = $("<p>").text(location);
-    //Main Weather Elements
-    var $mainWeather = $('<p id="main">').text(mainWeather);
+    var $main = $('<p id="main">').text(main);
     var $weatherIcon = $('<img>');
     $weatherIcon.attr({ 
       src: 'http://openweathermap.org/img/w/'+icon+'.png', 
       alt:" weather icon" 
     });
-    var $iconSup = $('<sup>').append($weatherIcon);
-    var $mainWithSup = $mainWeather.append($iconSup);
-    //Temprature Elements
-    var $temp = $('<p id="temp">').text(Math.round(temperature));  
-    var $tempSup = $('<sup id ="tempSup">');
     
+    var $iconSup = $('<sup>').append($weatherIcon);
+    var $mainWithSup = $main.append($iconSup);
+    var $temp = $('<p id="temp">').text(Math.round(tmp)); 
+    $temp.val(Math.round(tmp)); // use to convert
+    var $tempSup = $('<sup id ="tempSup">');
     var $btn = $('<button>').text('°C');
     $btn.attr( 'class', 'btnSup');
-
     $tempSup.append($btn);
+    
     var $tempWithSup = $temp.append($tempSup);
-    //Description
     var $description = $('<p>').text(description);
-    //Append the created Elements
     $weatherInfo.append($location);
     $weatherInfo.append($mainWithSup);
     $weatherInfo.append($tempWithSup);
     $weatherInfo.append($description);
     $weather.append($weatherInfo);
-    //Switch btn °F and °C
-    var bool = false; 
-    $weather.on('click','#tempSup',function(event) {    
-      var elem = $(this); 
-      bool = !bool;     
-      temperature = bool?(temperature*1.8)+32:(temperature-32)/1.8;
-      $temp.text(Math.round(temperature)).append(elem);
-      $btn.text(bool?'°F':'°C');   
-    });
+  },
 
+  displayWeatherDetails: function(temp_max,temp_min,humidity,pressure){
+    
+    // Clear div
+    var $details = $('#details').html('');
     //Details
     //ADD WINDD SPEED
     var $weatherDetails = $('<div>');
-    var $temp_max = $("<p>").text('Max Temp: '+(current.main.temp_max-273.15).toFixed());
-    var $temp_min = $("<p>").text('Min Temp: '+(current.main.temp_min-273.15).toFixed());
-    var $humidity = $("<p>").text('Humidity: '+current.main.humidity+'%');
-    var $pressure = $("<p>").text('Pressure: '+current.main.pressure+' hPa');
+    var $temp_max = $("<p>").text('Max Temp: '+temp_max);
+    var $temp_min = $("<p>").text('Min Temp: '+temp_min);
+    var $humidity = $("<p>").text('Humidity: '+humidity+'%');
+    var $pressure = $("<p>").text('Pressure: '+pressure+' hPa');
     $weatherDetails.append($temp_max.append($('<sup>').text('°C')));
     $weatherDetails.append($temp_min.append($('<sup>').text('°C')));
     $weatherDetails.append($humidity);
     $weatherDetails.append($pressure);
     $details.append($weatherDetails);
+  },
 
-    //Forecasting
+  displayForecastData: function(forecast) {
+    
+    var $icons = $('.icons').html('');
     var $days = $('.weekdays').html('');
-    var $icons = $('.icons').html(''); 
     
-    var day = new Date().getDay();
-    var list = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-    var spliced = list.splice(day);
-    var sorted = spliced.concat(list);
     
-    for(var i = 0; i<7; i++){
-      $days.append($('<li>').text(sorted[i]));
-    }
+    //Sort days in order starting from today
+    var sortedDays;
+    (function (){
+      let list = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+      let day = new Date().getDay();
+      let spliced = list.splice(day);
+      sortedDays = spliced.concat(list);
+    })();
     
+
+    // Update icon list
     for(var i = 0; i<forecast.list.length; i++){   
       var $imageList = $('<li>');
       var $img = $('<img>');
+     
       $imageList.append($img)
       $icons.append($imageList);
       $img.attr( { 
         src: 'http://openweathermap.org/img/w/'+forecast.list[i].weather[0].icon+'.png', 
         alt:" weather icon" 
       });
-    } 
-  }
-  
 
-  var $cityInput = $('#cityInput');
-  $cityInput.on('keypress', function (event) {
-    let city = $cityInput.val();			
-		if(event.keyCode === 13 && city){					 
-		  getCityBasedWeather(city);	
-		  $cityInput.val('');
-	  }
-	});
- 
-  //Use clientGeoLocator, if not activated - use ipGeoLocator as a "fallback" 
-  navigator.geolocation.getCurrentPosition(clientGeoLocator,ipGeoLocator); 
+      $days.append($('<li>').text(sortedDays[i]));
+    } 
+  },
+
+  setUpEventListeners: function(){
+
+    var bool = false; 
+    var $cityInput = $('#cityInput');
+    $cityInput.on('keypress', function (event) {
+      let city = $cityInput.val();      
+      if(event.keyCode === 13 && city){          
+        handler.getCityWeather(city);  
+        $cityInput.val('');
+      }
+    });
+   
+    $('#weather').on('click','#tempSup',function(event) { 
+      //Switch from °C-°F vice versa
+      bool = !bool;
+      $('.btnSup').text(bool?'°F':'°C');   
+    });   
+  }
 };
 
-$(document).ready(main);
+//Use clientGeoLocator, if not activated - use ipGeoLocator as a "fallback" 
+navigator.geolocation.getCurrentPosition(clientGeoLocator,ipGeoLocator); 
+
+function clientGeoLocator(position){      
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  handler.getGeoWeather(lat,long);  
+}
+
+function ipGeoLocator (){
+  $.get("http://ipinfo.io", function(response) {    
+    var location = response.loc.split(/,/);
+    let lat = location[0];
+    let long = location[1];
+    handler.getGeoWeather(lat,long);
+  },"jsonp");
+}
+
+view.setUpEventListeners();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
