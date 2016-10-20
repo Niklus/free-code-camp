1f18f2510d25b4579a21e65126af510f266f4c98
Nicholas <niklasoti@gmail.com>
2016-10-20 13:36:40 +0200

    view fixed

--- frontEnd/twitchTV/js/view.js
+++ frontEnd/twitchTV/js/view.js
@@ -6,7 +6,7 @@ var view = {
 
   init: function() {
     
-    $('#search').on('keypress', function (event) {      
+    $('#search').on('keypress', function (e) {      
       
       var channel = $(this).val();
       
@@ -16,15 +16,15 @@ var view = {
       }
     });
 
-    $('#all').on('click', function (event) {  
+    $('#all').on('click', function (e) {  
       ctrl.getAll();
     });
 
-    $('#online').on('click', function (event) {
+    $('#online').on('click', function (e) {
       ctrl.getOnline();
     });
 
-    $('#offline').on('click', function (event) {
+    $('#offline').on('click', function (e) {
       ctrl.getOffline();
     }); 
 
@@ -33,54 +33,66 @@ var view = {
 
   render: function(data) {
 
-    
     var channels = $('.list-group').html('');
 
-    data.forEach(function(obj){
+    data.forEach(function(obj){  
+      var list = view.createMediaList(obj);
+      channels.append(list);
+    });
+  },
 
-      var list = $('<li>').attr('class','list-group-item');
-      var container = $('<div>').attr('class','container');
-      var media = $('<div>').attr('class','media');
-      var styleLink = $('<a>').attr('class', 'media-left');
-      
-      var trueLink = $('<a>').attr({  
-        href:  obj.stream ? obj.stream.channel.url : obj.url,
-        target: '_blank'
-      });
+  createMediaList: function(obj) {
 
-      var img = $('<img>').attr({
+    var list = $('<li>').attr('class','list-group-item');
+    var link = $('<a>').attr({  
+      href:  obj.stream ? obj.stream.channel.url : obj.url,
+      target: '_blank'
+    });
+    var container = $('<div>').attr('class','container');
+    var media = view.createMedia(obj);
+
+    container.append(media);
+    link.append(container);
+    list.append(link);
+    return list;
+  },
+
+  createMedia: function(obj) {
+
+    var media = $('<div>').attr('class','media');
+    var styleLink = $('<a>').attr('class', 'media-left');
+    var img = $('<img>').attr({
         src: obj.stream ? obj.stream.channel.logo : obj.logo,
         class: 'media-object',
         alt: 'channels logo'
-      });
-
-      var mediaBody = $('<div>').attr('class','media-body');
-      var heading = $('<h4>').attr('class','media-heading');
-      heading.text(obj.stream ? obj.stream.game : 'offline');
-
-      var text = $('<p>');
-          
-      if(obj.status == 404){
-        text.text(obj.message);
-      }else if(obj.stream == null){
-        text.text(obj.status? obj.status : '...');
-      }else{
-        text.text(obj.stream.channel.status);
-      }
-
-      styleLink.append(img);
-
-      mediaBody.append(heading);
-      mediaBody.append(text);
-
-      media.append(styleLink);
-      media.append(mediaBody);
-
-      container.append(media);
-      trueLink.append(container);
-      list.append(trueLink);
-
-      channels.append(list);
     });
+    styleLink.append(img);
+    var mediaBody = this.createMediaBody(obj)
+
+    media.append(styleLink);
+    media.append(mediaBody);
+
+    return media;
+  },
+
+  createMediaBody: function(obj) {
+
+    var mediaBody = $('<div>').attr('class','media-body');
+    var heading = $('<h4>').attr('class','media-heading');
+    heading.text(obj.stream ? obj.stream.game : 'offline');
+    var text = $('<p>');
+          
+    if(obj.status == 404){
+      text.text(obj.message);
+    }else if(obj.stream == null){
+      text.text(obj.status? obj.status : '...');
+    }else{
+      text.text(obj.stream.channel.status);
+    }
+
+    mediaBody.append(heading);
+    mediaBody.append(text);
+
+    return mediaBody;
   }
 };
